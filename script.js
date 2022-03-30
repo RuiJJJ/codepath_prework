c382c30dfc85aadd478b913df8db0aebac4e9aa8
Glitch (galvanized-literate-hummingbird) <none>
2022-03-30 17:13:33 +0000

    🔭📕 Updated with Glitch

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 // global constants
-const clueHoldTime = 1000; //how long to hold each clue's light/sound
-const cluePauseTime = 333; //how long to pause in between clues
+const clueHoldTime = 500; //how long to hold each clue's light/sound
+const cluePauseTime = 100; //how long to pause in between clues
 const nextClueWaitTime = 1000;//how long to wait before starting playback of the clue sequence
 //Global Variables
-var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+//var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var pattern = [1,5,2,4,3];
 var progress = 0; 
 var gamePlaying = false;
 var tonePlaying = false;
@@ -32,12 +33,15 @@ function stopGame(){
 
 // Sound Synthesis Functions
 const freqMap = {
-  1: 261.6,
-  2: 329.6,
-  3: 392,
-  4: 466.2
+  1: 200.6,
+  2: 300.6,
+  3: 400.6,
+  4: 500.6,
+  5: 600.6
 }
-function playTone(btn,len){ 
+
+function playTone(btn,len){
+  
   o.frequency.value = freqMap[btn]
   g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
   context.resume()
@@ -48,7 +52,7 @@ function playTone(btn,len){
 }
 function startTone(btn){
   if(!tonePlaying){
-    context.resume()
+    context.resume()    
     o.frequency.value = freqMap[btn]
     g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
     context.resume()
@@ -90,7 +94,7 @@ function playClueSequence(){
   guessCounter = 0;
   context.resume()
   let delay = nextClueWaitTime; //set delay to initial wait time
-  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+  for(let i=0;i<=progress;i++){// for each clue that is revealed so far
     console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
     setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
     delay += clueHoldTime 
